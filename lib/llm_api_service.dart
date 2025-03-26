80fffe5ea72ee75d0c90a5aa11237efc131e07ea
yuju-k <112925490+yuju-k@users.noreply.github.com>
2025-03-26 19:52:27 +0900

    디바이스 설정 추가 및 API 키 변경

--- lib/llm_api_service.dart
+++ lib/llm_api_service.dart
@@ -5,7 +5,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 //프로젝트 최상위 폴더에 .env 파일을 추가하세요.
 //-- .env 파일 내용 --
-// GOOGLE_API_KEY=your_azure_api_key_here
+// GOOGLE_API_KEY=your_google_api_key_here
 // GOOGLE_NLP_ENDPOINT=https://language.googleapis.com/v2/documents:analyzeSentiment
 //
 
@@ -20,16 +20,13 @@ class GoogleNLPService {
       'Content-Type': 'application/json',
     };
     final requestBody = jsonEncode({
-      'document': {
-        'type': 'PLAIN_TEXT',
-        'content': text,
-        'language': 'ko'
-      },
+      'document': {'type': 'PLAIN_TEXT', 'content': text, 'language': 'ko'},
       'encodingType': 'UTF8'
     });
 
     try {
-      final response = await http.post(uri, headers: headers, body: requestBody);
+      final response =
+          await http.post(uri, headers: headers, body: requestBody);
       if (response.statusCode == 200) {
         final responseJson = jsonDecode(response.body);
         double score = responseJson['documentSentiment']['score'];
@@ -55,10 +52,12 @@ class MessageGenerationService {
   late final GenerativeModel _model;
 
   MessageGenerationService() {
-    _model = FirebaseVertexAI.instance.generativeModel(model: 'gemini-1.5-flash');
+    _model =
+        FirebaseVertexAI.instance.generativeModel(model: 'gemini-1.5-flash');
   }
 
-  Future<String> generateResponse(String message, List<String> previousMessages) async {
+  Future<String> generateResponse(
+      String message, List<String> previousMessages) async {
     try {
       final prompt = [
         Content.text(
@@ -67,8 +66,7 @@ class MessageGenerationService {
 이전 대화:
 ${previousMessages.join('\n')}
 
-현재 메시지: $message'''
-        )
+현재 메시지: $message''')
       ];
 
       final response = await _model.generateContent(prompt);
